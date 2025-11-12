7d0fc268d6baf18ce219365a99ade631a50ce2b0
vchernov <chernovitaly@gmail.com>
2025-11-11 22:31:34 -0500

    sending request to api onsuccess form submit

--- src/js/script.js
+++ src/js/script.js
@@ -120,7 +120,21 @@ try {
 					.querySelector("#checkbox")
 					.parentElement.parentElement.querySelector(".checkbox-error-message"),
 			}
-		);
+		)
+		.onSuccess((event) => {
+			const form = event.currentTarget;
+			const formData = new FormData(form);
+
+			fetch("https://httpbin.org/post", {
+				method: "POST",
+				body: formData,
+			})
+				.then((res) => res.json())
+				.then((data) => {
+					console.log("Success", data);
+					form.reset();
+				});
+		});
 } catch (e) {}
 
 try {
@@ -155,5 +169,19 @@ try {
 					.querySelector("#footer__checkbox")
 					.parentElement.parentElement.querySelector(".check-error-message"),
 			}
-		);
+		)
+		.onSuccess((event) => {
+			const form = event.currentTarget;
+			const formData = new FormData(form);
+
+			fetch("https://httpbin.org/post", {
+				method: "POST",
+				body: formData,
+			})
+				.then((res) => res.json())
+				.then((data) => {
+					console.log("Success", data);
+					form.reset();
+				});
+		});
 } catch (e) {}
